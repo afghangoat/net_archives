2b5b4883a308241ce1b9af29ea6ab319515d2c5d
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-07-22 10:57:29 +0200

    Initial commit - Mirrored from my site, file: .Net_archives/Cursortest/Cursortest/Form1.vb

---
+++ .Net_archives/Cursortest/Cursortest/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        While True
+            Cursor.Position = New Point(TextBox1.Text)
+        End While
+
+
+    End Sub
+End Class
