d281468845d8bcd9ab2854359cb45bf9912f10e5
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-07-22 10:37:44 +0200

    Initial commit - Mirrored from my site, file: .Net_archives/Atlagolo/Atlagolo/Form1.vb

---
+++ .Net_archives/Atlagolo/Atlagolo/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Dim SUM As String
+    Dim NUM As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        NUM = (TextBox6.Text + TextBox7.Text + TextBox8.Text + TextBox9.Text + TextBox10.Text)
+        SUM = ((5 * TextBox6.Text) + (4 * TextBox7.Text) + (3 * TextBox8.Text) + (2 * TextBox9.Text) + (1 * TextBox10.Text)) / NUM
+        Label4.Text = SUM
+    End Sub
+End Class
