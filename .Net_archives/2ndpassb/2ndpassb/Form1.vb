bf03f3a28e725f34e672643dcb984b745164dfc2
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-07-22 10:15:45 +0200

    Initial commit - Mirrored from my site, file: .Net_archives/2ndpassb/2ndpassb/Form1.vb

---
+++ .Net_archives/2ndpassb/2ndpassb/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Dim code = 1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = code Then
+            Me.Close()
+        Else
+            MsgBox("Wrong code!")
+        End If
+    End Sub
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        e.Cancel = True
+        MsgBox("Please login!")
+    End Sub
+End Class
