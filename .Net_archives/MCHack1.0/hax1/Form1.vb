b8378adc9ca9eb159e8a05e88470f7fd3ac182a6
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-07-23 10:51:43 +0200

    Initial commit - Mirrored from my site, file: .Net_archives/MCHack1.0/hax1/Form1.vb

---
+++ .Net_archives/MCHack1.0/hax1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Public Declare Sub mouse_event Lib "user32" Alias "mouse_event" (ByVal dwFlags As Long)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Threading.Thread.Sleep(220)
+        mouse_event(&H2)
+        mouse_event(&H4)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+End Class
