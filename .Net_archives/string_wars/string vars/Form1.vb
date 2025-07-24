a951e55ba7ebc5b50dc0a61d3fddea4e8deb7098
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-07-24 11:06:19 +0200

    Initial commit - Mirrored from my site, file: .Net_archives/string_wars/string vars/Form1.vb

---
+++ .Net_archives/string_wars/string vars/Form1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Threading
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dir1 As String = "abcdefghijklmnopqrstuvwxyz"
+        Dim di = 1
+        Dim ii As String
+        Dim iii As Integer
+        Thread.Sleep(100)
+        ii = Mid(dir1, iii + di, 1)
+        TextBox1.Text = ii
+        di += 1
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
