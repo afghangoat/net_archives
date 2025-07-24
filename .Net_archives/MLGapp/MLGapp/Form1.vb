de2d0ebac97f654c95b6df8568e77de8e993e2d5
AfghanGoat <ifj.funk.gabor@gmail.com>
2025-07-24 10:48:18 +0200

    Initial commit - Mirrored from my site, file: .Net_archives/MLGapp/MLGapp/Form1.vb

---
+++ .Net_archives/MLGapp/MLGapp/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Dim KoTimer As New PointToScreen()
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        While 1 = 1
+            DesktopLocation = New Point(KoTimer.Next(1, 1000), KoTimer.Next(1, 500))
+        End While
+    End Sub
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+
+    Private Class PointToScreen(Of)
+
+    End Class
+End Class
